a2386a7a6fd0f3e96e9e18ab4af858d0bad05f9a
hcrespo06 <hcrespo@frba.utn.edu.ar>
2023-05-03 23:03:25 -0300

    version final

--- 00-CHelloworld/helloworld.c
+++ 00-CHelloworld/helloworld.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 int main() {
-
-   FILE * archivo;
-   archivo = fopen ("output.txt", "w+");
-
    printf("Hello, World!");
-   fprintf(archivo, "Hello, World!");
-   
-   fclose(archivo);
    
    return 0;
 }
